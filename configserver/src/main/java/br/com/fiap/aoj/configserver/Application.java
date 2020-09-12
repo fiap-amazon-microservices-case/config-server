55d6d4be08b1549b8c760fa5668e3352960e29aa
rbcabrals <rcabral@4ward.com.br>
2020-09-11 21:56:06 -0300

    Ajuste de configurações do config server.

--- configserver/src/main/java/br/com/fiap/aoj/configserver/Application.java
+++ configserver/src/main/java/br/com/fiap/aoj/configserver/Application.java
@@ -2,9 +2,13 @@ package br.com.fiap.aoj.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 import static br.com.fiap.aoj.configserver.Application.BASE_PACKAGE;
 
+@EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = { BASE_PACKAGE })
 public class Application {
 
